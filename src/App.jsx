35085ab5942ccaf4cd411b6505274e29c0b3550c
dkoncius <as.koncius@gmail.com>
2023-07-24 11:12:25 +0300

    auth logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
-import { useState, useEffect } from "react"
-import { Vote } from "./components/Vote"
-import { getSongs, voteForSong } from './firebase';
+import { useState, useEffect } from "react";
+import { Vote } from "./components/Vote";
+import { getSongs, voteForSong, signInWithToken, signInWithGoogle, signOutUser, auth } from './firebase';
 
 function App() {
-  const [songs, setSongs] = useState([])
-  const [votes, setVotes] = useState({})
+  const [songs, setSongs] = useState([]);
+  const [votes, setVotes] = useState({});
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const fetchSongs = async () => {
+    const fetchSongsAndSignIn = async () => {
+      // Fetch songs
       const fetchedSongs = await getSongs();
       setSongs(fetchedSongs);
       setVotes(fetchedSongs.reduce((votes, song) => ({ ...votes, [song.id]: song.votes }), {}));
+
+      // Sign in user with custom token
+      const token = import.meta.env.VITE_APP_CUSTOM_TOKEN;
+      await signInWithToken(token);
     };
-    fetchSongs();
+    
+    fetchSongsAndSignIn();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => setUser(user));
+    return () => unsubscribe();  // clean up on unmount
   }, []);
 
   const handleClick = async (id) => {
     await voteForSong(id);
     setVotes({ ...votes, [id]: votes[id] + 1 });
-  }
+  };
+
+  const handleSignOut = async () => {
+    await signOutUser();
+  };
 
   // Sort the songs by vote count
-  const sortedSongs = [...songs].sort((a, b) => (votes[b.id] || 0) - (votes[a.id] || 0))
+  const sortedSongs = [...songs].sort((a, b) => (votes[b.id] || 0) - (votes[a.id] || 0));
+
+  if (!user) {
+    return (
+      <div>
+        <button onClick={signInWithGoogle}>Sign In with Google</button>
+      </div>
+    );
+  }
 
   return (
     <>
+      <button onClick={handleSignOut}>Sign Out</button>  {/* Add sign out button */}
       <h1>Balsuokite už patikusias giesmes</h1>
       <table border="1">
         <thead>
@@ -49,7 +74,7 @@ function App() {
         </tbody>
       </table>
     </>
-  )
+  );
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs, doc, updateDoc, increment, getDoc } from 'firebase/firestore';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, signInWithCustomToken, signInWithRedirect } from "firebase/auth";
+
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,6 +16,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth };
 
 export const getSongs = async () => {
   const snapshot = await getDocs(collection(db, 'songs'));
@@ -27,5 +34,35 @@ export const voteForSong = async (id) => {
     await updateDoc(songRef, { votes: increment(1) });
   } else {
     console.log(`No song with ID ${id} exists.`);
+  }
+};
+
+export const signInWithToken = async (token) => {
+  try {
+    const userCredential = await signInWithCustomToken(auth, token);
+    const user = userCredential.user;
+    console.log("Signed in user:", user);
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error('Error signing in with custom token:', errorMessage);
+  }
+};
+
+export const signInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  try {
+    await signInWithRedirect(auth, provider);
+  } catch (error) {
+    console.error('Error signing in with Google:', error);
+  }
+};
+
+export const signOutUser = async () => {
+  try {
+    await signOut(auth);
+    console.log('User signed out');
+  } catch (error) {
+    console.error('Error signing out:', error);
   }
 };
