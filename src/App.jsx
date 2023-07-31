8180a66c3f23a2dc3274a30825eebeef87857535
dkoncius <as.koncius@gmail.com>
2023-07-31 16:03:30 +0300

    ux/ui description

--- src/App.jsx
+++ src/App.jsx
@@ -31,13 +31,17 @@ function App() {
   return (
     <>
       {user ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={signInWithGoogle}>Sign In with Google</button>}
-      <h1>Balsuokite už patikusias giesmes</h1>
-      <p>Gražiausių Gabrielės Gvazdikaitės giesmių rinkimai
-      2024 metų vasarą šiuolaikinės krikščioniškos muzikos dainininkė Gabrielė Gvazdikaitė ruošia malonų siurprizą savo klausytojams - tai gražiausių atlikėjos giesmių koncertai su orkestru ir Berklee muzikos koledžą baigusiu pianistu Domu Žeromsku.</p>
+      
+      <div className="description">
+        <h1>Balsuokite už patikusias giesmes</h1>
+        <p>Gražiausių Gabrielės Gvazdikaitės giesmių rinkimai
+        2024 metų vasarą šiuolaikinės krikščioniškos muzikos dainininkė Gabrielė Gvazdikaitė ruošia malonų siurprizą savo klausytojams - tai gražiausių atlikėjos giesmių koncertai su orkestru ir Berklee muzikos koledžą baigusiu pianistu Domu Žeromsku.</p>
 
-      <p>Kokie Gabrielės kūriniai skambės šiuose išskirtiniuose koncertuose - gali nuspręsti ir tu! Balsuok už mėgstamiausias Gabrielės Gvazdikaitės giesmes ir rugpjūčio 31 dieną radijo stoties XFM Pavakario eteryje sužinok kokias 7 koncertuose su orkestru skambėsiančias giesmes išrinks klausytojai, o kokius 5 kūrinius - Gabrielė ir jos komanda!</p>
+        <p>Kokie Gabrielės kūriniai skambės šiuose išskirtiniuose koncertuose - gali nuspręsti ir tu! Balsuok už mėgstamiausias Gabrielės Gvazdikaitės giesmes ir rugpjūčio 31 dieną radijo stoties XFM Pavakario eteryje sužinok kokias 7 koncertuose su orkestru skambėsiančias giesmes išrinks klausytojai, o kokius 5 kūrinius - Gabrielė ir jos komanda!</p>
 
-      <p>Nepraleiskite progos ir dalyvaukite balsavime, o radijo stoties XFM klausytojų laukia malonūs prizai!</p>
+        <p>Nepraleiskite progos ir dalyvaukite balsavime, o radijo stoties XFM klausytojų laukia malonūs prizai!</p>
+      </div>
+     
 
       {user &&  
        <div className="songs-grid">
