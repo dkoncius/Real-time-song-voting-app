81071bb2206d9e0b1ebcf30a4edcbb25ca523442
dkoncius <as.koncius@gmail.com>
2023-07-31 18:28:32 +0300

    smooth loading

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from 'react';
 import { Vote } from "./Vote";
 import { voteForSong } from '../firebase';
+import { useInView } from 'react-intersection-observer';
+import Iframe from 'react-iframe'; // importing the react-iframe package
 
 const SongCard = ({ song, votes, user, setVotes, rank }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
+
+  const [iframeLoaded, setIframeLoaded] = useState(false); // new piece of state to track iframe loading
+
   const handleClick = async () => {
     if (user) {
       const success = await voteForSong(user.uid, song.id);
@@ -17,10 +25,18 @@ const SongCard = ({ song, votes, user, setVotes, rank }) => {
   };
 
   return (
-    <div className="song-card" onClick={handleClick}>
+    <div className="song-card" onClick={handleClick} style={{opacity: inView && 1}}>
       <div className="rank">{rank}</div>
-      <div>
-        <iframe src={song.src}></iframe>
+      
+      <div ref={ref}>
+        {inView && 
+        <div className="iframe-wrapper" style={{ opacity: iframeLoaded ? 1 : 0, transition: 'opacity 1s ease-in' }}>
+          <Iframe url={song.src} 
+            onLoad={() => setIframeLoaded(true)}
+            className="iframe"
+          />
+        </div>
+        }
       </div>
       <button className="votes-count">
         <Vote vote={votes[song.id] || 0}/>
