f909c7815a56c3cacf906fe72d7ab19f57ece22c
dkoncius <as.koncius@gmail.com>
2023-07-31 19:06:19 +0300

    balsai design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
+import Description from "./components/Description";
 import SongCard from "./components/SongCard";
-import { getSongs, signInWithGoogle, signOutUser, auth } from './firebase';
+import Login from "./components/Login";
+import { getSongs } from './firebase';
 
 function App() {
   const [songs, setSongs] = useState([]);
   const [votes, setVotes] = useState({});
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); // Move user state to App component
 
   useEffect(() => {
     const fetchSongs = async () => {
@@ -17,32 +19,14 @@ function App() {
     fetchSongs();
   }, []);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => setUser(user));
-    return () => unsubscribe();
-  }, []);
-
-  const handleSignOut = async () => {
-    await signOutUser();
-  };
-
   const sortedSongs = [...songs].sort((a, b) => (votes[b.id] || 0) - (votes[a.id] || 0));
 
   return (
     <>
-      {user ? <button onClick={handleSignOut}>Atsijungti</button> : <button onClick={signInWithGoogle}>Prisijungti su Google</button>}
+      <Login user={user} setUser={setUser}/> {/* Pass user state down as props */}
       
-      <div className="description">
-        <h1>Balsuokite už patikusias giesmes</h1>
-        <p>Gražiausių Gabrielės Gvazdikaitės giesmių rinkimai
-        2024 metų vasarą šiuolaikinės krikščioniškos muzikos dainininkė Gabrielė Gvazdikaitė ruošia malonų siurprizą savo klausytojams - tai gražiausių atlikėjos giesmių koncertai su orkestru ir Berklee muzikos koledžą baigusiu pianistu Domu Žeromsku.</p>
-
-        <p>Kokie Gabrielės kūriniai skambės šiuose išskirtiniuose koncertuose - gali nuspręsti ir tu! Balsuok už mėgstamiausias Gabrielės Gvazdikaitės giesmes ir rugpjūčio 31 dieną radijo stoties XFM Pavakario eteryje sužinok kokias 7 koncertuose su orkestru skambėsiančias giesmes išrinks klausytojai, o kokius 5 kūrinius - Gabrielė ir jos komanda!</p>
-
-        <p>Nepraleiskite progos ir dalyvaukite balsavime, o radijo stoties XFM klausytojų laukia malonūs prizai! P.s. turite 5 balsus per dieną :)</p>
-      </div>
+      <Description/>
      
-
       {user &&  
        <div className="songs-grid">
         {sortedSongs.map((song, index) => (
