6b99dd0be6decb59b0f6251a42c28de738a396ff
dkoncius <as.koncius@gmail.com>
2023-08-04 16:03:15 +0300

    done

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
+import titleImage from '../assets/title.png'
 
 const Description = () => {
   return (
-    <div className="description">
-        <h1>Gražiausių Gabrielės Gvazdikaitės giesmių rinkimai</h1>
-        <p>2024 metų vasarą šiuolaikinės krikščioniškos muzikos dainininkė Gabrielė Gvazdikaitė ruošia malonų siurprizą savo klausytojams - tai gražiausių atlikėjos giesmių koncertai su orkestru ir Berklee muzikos koledžą baigusiu pianistu Domu Žeromsku.</p>
+    <div>
+        <section className='description-title'>
+          <div className="left">
+            <h1>Gražiausių Gabrielės Gvazdikaitės giesmių rinkimai</h1>
+          </div>
+         
+          <div className="clip-path">
+            <img src={titleImage} alt="Gabriele Gvazdikaite" />
+          </div>
+         
+        </section>
 
-        <p>Kokie Gabrielės kūriniai skambės šiuose išskirtiniuose koncertuose - gali nuspręsti ir tu! Balsuok už mėgstamiausias Gabrielės Gvazdikaitės giesmes ir rugpjūčio 31 dieną radijo stoties XFM Pavakario eteryje sužinok kokias 7 koncertuose su orkestru skambėsiančias giesmes išrinks klausytojai, o kokius 5 kūrinius - Gabrielė ir jos komanda!</p>
+        <section className='description-text'>
+          <div className="container">
+            <h2>Apie</h2>
+            <p>2024 metų vasarą šiuolaikinės krikščioniškos muzikos dainininkė Gabrielė Gvazdikaitė ruošia malonų siurprizą savo klausytojams - tai gražiausių atlikėjos giesmių koncertai su orkestru ir Berklee muzikos koledžą baigusiu pianistu Domu Žeromsku.</p>
 
-        <p>Nepraleiskite progos ir dalyvaukite balsavime, o radijo stoties XFM klausytojų laukia malonūs prizai! P.s. turite 5 balsus per dieną :)</p>
+            <p>Kokie Gabrielės kūriniai skambės šiuose išskirtiniuose koncertuose - gali nuspręsti ir tu! Balsuok už mėgstamiausias Gabrielės Gvazdikaitės giesmes ir rugpjūčio 31 dieną radijo stoties XFM Pavakario eteryje sužinok kokias 7 koncertuose su orkestru skambėsiančias giesmes išrinks klausytojai, o kokius 5 kūrinius - Gabrielė ir jos komanda!</p>
+
+            <p>Nepraleiskite progos ir dalyvaukite balsavime, o radijo stoties XFM klausytojų laukia malonūs prizai!</p>
+          </div>
+        </section>
+        
     </div>
   )
 }
--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,49 +1,38 @@
-import React, { useState, useEffect, memo, useMemo } from 'react';
+import React, { useState, useMemo } from 'react';
 import { voteForSong } from '../firebase';
-import { useInView } from 'react-intersection-observer';
 import Iframe from 'react-iframe';
 
-const SongCard = memo(({ song, votes, user, setVotes, rank }) => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-  });
-
+const SongCard = ({ song, votes, user, setVotes, rank }) => {
   const [iframeLoaded, setIframeLoaded] = useState(false);
   const songVotes = useMemo(() => votes[song.id] || 0, [votes, song.id]);
 
   const handleClick = async () => {
-    if (user) {
-      const success = await voteForSong(user.uid, song.id);
-      if (success) {
-        setVotes(prevVotes => ({ ...prevVotes, [song.id]: (prevVotes[song.id] || 0) + 1 }));
-      } else {
-        console.log("Vote was not successful.");
-      }
+    const success = await voteForSong(user.uid, song.id);
+    if (success) {
+      setVotes(prevVotes => ({ ...prevVotes, [song.id]: (prevVotes[song.id] || 0) + 1 }));
     } else {
-      console.log("User is not signed in.");
+      console.log("Vote was not successful.");
     }
   };
 
   return (
-    <div className="song-card" onClick={handleClick} style={{opacity: inView ? 1 : 0.5}}>
-      
-      <div ref={ref}>
-        {inView && 
-        <div className="iframe-wrapper" style={{ opacity: iframeLoaded ? 1 : 0, transition: 'opacity 1s ease-in' }}>
-          <div className="rank">{rank}</div>
-          <Iframe url={song.src} 
-            onLoad={() => setIframeLoaded(true)}
-            className="iframe"
-          />
-          <button className="votes-count">
-            <i className="fa-solid fa-thumbs-up"></i>
-            <p>{songVotes}</p>
-          </button>
-        </div>
-        }
+    <div className="song-card" 
+      onClick={user ? handleClick : console.log("User is not signed in.")} 
+      style={{opacity: iframeLoaded ? 1 : 0.5}}
+    >
+      <div className="iframe-wrapper" style={{ opacity: iframeLoaded ? 1 : 0, transition: 'opacity 1s ease-in' }}>
+        <div className="rank">{rank}</div>
+        <Iframe url={song.src} 
+          onLoad={() => setIframeLoaded(true)}
+          className="iframe"
+        />
+        <button className="votes-count">
+          <i className="fa-solid fa-heart"></i>
+          <p>{songVotes}</p>
+        </button>
       </div>
     </div>
   );
-});
+};
 
 export default SongCard;
